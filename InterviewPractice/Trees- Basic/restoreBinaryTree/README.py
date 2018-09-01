879d3b332849bc1029a48ee430a514609f1991b0
kah-ve <cam.abdurrahman@gmail.com>
2018-09-01 00:11:33 -0400

    Update README.py

--- InterviewPractice/Trees- Basic/restoreBinaryTree/README.py
+++ InterviewPractice/Trees- Basic/restoreBinaryTree/README.py
@@ -4,5 +4,15 @@ Let's define inorder and preorder traversals of a binary tree as follows:
 
 Inorder traversal first visits the left subtree, then the root, then its right subtree;
 Preorder traversal first visits the root, then its left subtree, then its right subtree.
+For example, if tree looks like this:
 
+    1
+   / \
+  2   3
+ /   / \
+4   5   6
+then the traversals will be as follows:
+
+Inorder traversal: [4, 2, 1, 5, 3, 6]
+Preorder traversal: [1, 2, 4, 3, 5, 6]
 Given the inorder and preorder traversals of a binary tree t, but not t itself, restore t and return it.
