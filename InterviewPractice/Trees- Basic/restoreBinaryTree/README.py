86b4f93bea22dfe5f8ab1ef0419fc23683b7c8fc
kah-ve <cam.abdurrahman@gmail.com>
2018-09-01 00:11:21 -0400

    Create README.py

---
+++ InterviewPractice/Trees- Basic/restoreBinaryTree/README.py
@@ -0,0 +1,8 @@
+Note: Your solution should have O(inorder.length) time complexity, since this is what you will be asked to accomplish in an interview.
+
+Let's define inorder and preorder traversals of a binary tree as follows:
+
+Inorder traversal first visits the left subtree, then the root, then its right subtree;
+Preorder traversal first visits the root, then its left subtree, then its right subtree.
+
+Given the inorder and preorder traversals of a binary tree t, but not t itself, restore t and return it.
